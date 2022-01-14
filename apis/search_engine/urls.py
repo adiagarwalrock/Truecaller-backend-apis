64ca5eb02c829abcbe35a61359015b1e9f8c7e48
Aditya Agarwal <aditya.ag1234@gmail.com>
2022-01-15 00:52:26 +0530

    global search

--- apis/search_engine/urls.py
+++ apis/search_engine/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import ListUserAPIView
 from .views import ListUserContactAPIView
-from .views import global_search_view
+from .views import global_search_view, SearchForUsers, GlobalSearchView
 
 """ URLS --> serach_engine """
 
@@ -11,5 +11,7 @@ urlpatterns = [
 
     path('user/', ListUserAPIView.as_view()),
 
-    path('global/',global_search_view)
+    path('global/', GlobalSearchView.as_view()),
+
+
 ]
--- apis/search_engine/views.py
+++ apis/search_engine/views.py
@@ -1,34 +1,62 @@
+from django.http.response import JsonResponse
+from django import template
+from django.db.models import Q
+from django.views.generic import ListView
+
 from rest_framework.generics import ListAPIView
 from rest_framework.filters import SearchFilter
 
+from itertools import chain
+
 from users.models import User, UserContact
 from users.serializers import UserSerializer, UserContactSerializer
+from users.serializers import UserSearchSerializer, ContactSearchSerializer
 
 '''Search_Engine --> Views'''
 
 
 class ListUserContactAPIView(ListAPIView):
-    queryset = UserContact.objects.all()
-    serializer_class = UserContactSerializer
     filter_backends = [SearchFilter]
+    queryset = UserContact.objects.all()
+    serializer_class = ContactSearchSerializer
     search_fields = ['phone', 'name']
 
 
-class GlobalSearch(ListAPIView):
-    pass
+class GlobalSearchView(ListView):
 
-
-class DynamicSearchFilter(SearchFilter):
-
-    def get_search_fields(self, view, request):
-        return request.GET.getlist('search_fields', [])
+    def get_queryset(self):
+        request = self.request
+        query = request.GET.get('q', None)
+        if query is not None:
+            user_results = User.objects.search(query=query)
+            contact_results = UserContact.objects.search(query=query)
+            queryset_chain = chain(
+                user_results,
+                contact_results
+            )
+            query_search = sorted(queryset_chain,
+                                  key=lambda instance: instance.pk,
+                                  reverse=True)
+            self.count = len(query_search)
+            return JsonResponse({"results": list(query_search)}, safe=False)
+        return User.objects.none()
 
 
 class ListUserAPIView(ListAPIView):
-    filter_backends = [DynamicSearchFilter]
+    filter_backends = [SearchFilter]
     queryset = User.objects.all()
-    serializer_class = UserSerializer
+    serializer_class = UserSearchSerializer
     search_fields = ['phone', 'name']
+
+
+class SearchForUsers(ListAPIView):
+    serializer_class = ContactSearchSerializer
+
+    def get_queryset(self):
+        name = self.kwargs['name']
+        queryset = UserContact.objects.all()
+        queryset_starts_with = queryset.filter(name__istartswith=name)
+        queryset_contains = queryset.filter(name__icontains=name)
 
 
 def global_search_view():
